74f016424665c9c0eed410310ae6933d961c9094
fy1128 <892973009@qq.com>
2019-05-05 15:53:30 +0800

    add aria2

---
+++ aria2/tracker-updator.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#more trackers list, see https://github.com/ngosang/trackerslist
+
+tracker_url='https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt'
+path='http://127.0.0.1:6800/jsonrpc'
+#passwd='xxxxxx'
+
+tracker=$(echo -n  $(curl -s -L $tracker_url | sed 'N;s/\n//g') | tr ' ' ',')
+#[ -n "$tracker" ] && curl $path -d '{"jsonrpc":"2.0","method":"aria2.changeGlobalOption","id":"cron","params":["token:'$passwd'",{"bt-tracker":"'$tracker'"}]}'
+[ -n "$tracker" ] && curl $path -d '{"jsonrpc":"2.0","method":"aria2.changeGlobalOption","id":"cron","params":[{"bt-tracker":"'$tracker'"}]}'
